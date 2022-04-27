669d44dddbee0802548a25cf1383e1f80b989fad
unknown <gespejo2144@gmail.com>
2022-04-26 19:55:28 -0700

    added map and buisness bar

---
+++ public/map.js
@@ -0,0 +1,10 @@
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
+
+// Add OpenStreetMap tiles:
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
